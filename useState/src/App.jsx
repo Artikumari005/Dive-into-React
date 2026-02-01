8907647195085b5df99a19213894822063659270
Aarti kumari <k8327890@gmail.com>
2026-02-01 17:46:30 +0530

    useState

---
+++ useState/src/App.jsx
@@ -0,0 +1,40 @@
+
+import React from "react"
+import './App.css'
+
+
+/*function App() {
+ const [text , setText]= React.useState("hello")
+ function handleClick(){
+  setText("Hiii")
+ }
+ return(
+  <main>
+    <h1>state is import</h1>
+    <button onClick={handleClick} >{text}</button>
+  </main>
+ )
+  
+} */
+function App(){
+  
+  const[count , setCount]= React.useState(0)
+  function handleMinus(){
+    setCount(preCount=>preCount-1)
+  }
+  function handlePlus(){
+    setCount(preCount => preCount+1)
+  }
+  return(
+    <main className="container">
+      <h1>How many times Zennie say "state" in this section?</h1>
+      <div className="counter">
+        <button onClick={handleMinus} className="minus" aria-label="Decrese count">-</button>
+        <h2 className="count">{count}</h2>
+        <button onClick={ handlePlus} className="plus" aria-label="Increase count">+</button>
+      </div>
+    </main>
+  )
+}
+
+export default App
