37f2634664f3901a58dfc78517d4fd374bb7f33d
Aarti kumari <k8327890@gmail.com>
2026-02-10 22:30:12 +0530

    conditional rendering

--- React-Project3/src/Jokes.jsx
+++ React-Project3/src/Jokes.jsx
@@ -1,5 +1,12 @@
 import './index.css'
+import React from 'react'
 export default function Jokes(props){
+
+const [isShow , setIsShow] = React.useState(false)
+function toggleShow(){
+    setIsShow(preShow => !preShow)
+}
+
     {/*console.log(!props.isPun)*/}
     console.log(props.comments)
     return(
@@ -8,7 +15,8 @@ export default function Jokes(props){
      {/*   { props.setup &&<p className="setup"> Setup{props.setup}</p>}*/}
          <p style={{display:props.setup? "block":"none"}}   className="setup"> Setup{props.setup}</p>
 
-        <p className="punchline">{props.punchline}</p>
+        {isShow && <p className="punchline">{props.punchline}</p>}
+        <button onClick={toggleShow}>{isShow ? "hide" :"show" } punchline</button>
         <hr className="line"></hr>
         </>
     )
