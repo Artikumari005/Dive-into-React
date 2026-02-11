077b3817b21cc730d153a9995427a3636f66f397
Aarti kumari <k8327890@gmail.com>
2026-02-11 22:52:24 +0530

    chefclaude project

--- ChefClaude/src/Main.jsx
+++ ChefClaude/src/Main.jsx
@@ -6,18 +6,17 @@ export default function Main(){
    
     
     const [ingredient ,setIngredient]= React.useState([])
-    const ingredientListItems = ingredient.map(ingredient=>(
+    const ingredientsListItems = ingredient.map(ingredient=>(
         <li key={ingredient}>{ingredient}</li>
     ))
 
-    function addIngredient(formData){
-        
+    function addIngredient(formData){ 
        const newIngredient = formData.get("ingredient")
        setIngredient(preIngredients => [ ...preIngredients , newIngredient])
       
     }
     return(
-        <main>
+        <main >
             <form action={addIngredient} className='add-ingredient-form'>
                 <input
                 type="text"
@@ -26,9 +25,18 @@ export default function Main(){
                 name='ingredient'></input>
                 <button className='button' >Add ingredient</button>
             </form>
-            <ul>
-                {ingredientListItems}
-            </ul>
+          {ingredient.length > 0 &&  <section className='list-ingredient' >
+            <h1>Ingredients on Hands:</h1>
+            <ul className='ingredients-list' arial-alive="polite">{ingredientsListItems}</ul>
+             {ingredient.length > 3 && <div className='get-recipe-container'>
+                <div>
+                    <h3>Ready fo a recipe</h3>
+                    <p>Generate a recipe your list of ingredients.</p>
+                </div>
+                <button>Get a recipe</button>
+             </div>}
+
+           </section>}
         </main>
     )
 }
