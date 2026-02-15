e31a70d39e6dae45ab904eb67fe78b0315023fb3
Aarti kumari <k8327890@gmail.com>
2026-02-15 20:43:14 +0530

    .

---
+++ Meme/Components/Header.jsx
@@ -0,0 +1,9 @@
+import '/src/index.css'
+export default function Header(){
+    return (
+        <div className="navbar">
+            <img src="./src/meme.jpg" className='logo' alt='logo'></img>
+            <h1>Meme Generator</h1>
+        </div>
+    )
+}
---
+++ Meme/Components/Main.jsx
@@ -0,0 +1,50 @@
+import React from "react"
+
+export default function Main(){
+    const [meme , setMeme]= React.useState({
+        topText:"One does not simply",
+        bottomText:"walk into mordor",
+        imgUrl:"http://i.imgflip.com/1bij.jpg"
+    })
+    
+    function handleChange(event){
+       const {value, name} =event.currentTarget
+       setMeme(preMeme => ({
+        ...preMeme,
+        [name]:value,
+        
+       }))
+      
+    }
+    return (
+        <section className="main-container">
+            <div className="form">
+              <label>Top Text
+                    <input
+                        type="text"
+                        placeholder="One does not simply"
+                        name="topText"
+                        onChange={handleChange}
+                        value={meme.topText}
+                    />
+                </label>
+
+                <label>Bottom Text
+                    <input
+                        type="text"
+                        placeholder="Walk into Mordor"
+                        name="bottomText"
+                        onChange={handleChange}
+                        value={meme.bottomText}
+                    />
+                </label>
+            </div>
+            <button  className="get-button">Get new meme image</button>
+             <div className="meme">
+                <img src={meme.imgUrl} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
+            </div>
+        </section>
+    )
+}
