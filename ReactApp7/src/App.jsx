33307e87a5a956898724693036f43bcd7469b322
Aarti kumari <k8327890@gmail.com>
2026-02-17 22:47:39 +0530

    .

--- ReactApp7/src/App.jsx
+++ ReactApp7/src/App.jsx
@@ -1,13 +1,20 @@
 import React from "react"
 
 export default function App(props) {
-    const [starWarsData, setStarWarsState] = React.useState(null)
-    fetch("https://swapi.dev/api/people/1")
+    const [starWarsData, setStarWarsState] = React.useState({})
+     const [count, setCount] = React.useState(0)
+    
+
+    React.useEffect(function(){
+        fetch("https://swapi.dev/api/people/1")
         .then(res => res.json())
-        .then(data => setStarWarsState(data))
+        //.then(data => setStarWarsState(data))
+    } , [count])
     
     return (
         <div>
+            <h2>The count is {count}</h2>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
             <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
         </div>
     )
