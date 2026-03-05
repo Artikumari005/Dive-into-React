037f69eafec01b42592c012935a7cc4cf5ecabbb
Aarti kumari <k8327890@gmail.com>
2026-03-05 15:37:24 +0530

    .

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -1,2 +1,13 @@
-export { default as Header } from "./Header/Header";
-export { default as Footer } from "./Footer/Footer";
+import Header from './Header/Header'
+import Footer from './Footer/Footer'
+import Container  from './container/Container'
+import Logo from './Logo'
+import LogoutBtn from './Header/LogoutBtn'
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
+}
